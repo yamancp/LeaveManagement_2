c013ade16dec7403195a81e27a3ba1e9d3897277
yamancp <yamancp@yahoo.com>
2022-09-01 05:46:17 -0500

    Updated Leave tables with base entity class.

---
+++ LeaveManagement_2/Data/LeaveAllocation.cs
@@ -0,0 +1,15 @@
+﻿namespace LeaveManagement_2.Data
+{
+    public class LeaveAllocation: BaseEntity
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }   
+        public LeaveType LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }    
+
+        public string EmployeeId { get; set; }  
+        public DateTime DateCreated { get; set; }   
+        public DateTime DateModified { get; set; }
+    }
+}
--- LeaveManagement_2/Data/LeaveType.cs
+++ LeaveManagement_2/Data/LeaveType.cs
@@ -1,6 +1,12 @@
 ﻿namespace LeaveManagement_2.Data
 {
-    public class LeaveType
+    public class LeaveType : BaseEntity
     {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public int DefaultDays { get; set; }    
+        public DateTime DateCreated { get; set; }   
+        public DateTime DateModified    { get; set; }
+
     }
 }
